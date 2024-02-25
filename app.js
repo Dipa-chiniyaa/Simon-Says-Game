b25d44ab2ac40dc4668a01ce88f123de6bca8a04
Dipa Chiniya Parasaram <157943824+Dipa-chiniyaa@users.noreply.github.com>
2024-02-25 11:54:35 +0530

    final code of  app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let gameSeq = [];
 let userSeq = [];
+let highScore = 0 ;
 
 let btns = ['yellow','purple', 'red', 'green']; // manipulating the buttons by their classes
 
@@ -18,7 +19,14 @@ document.addEventListener('keypress', function () {
     }
 });
 
-function btnFlash(btn) {
+function userFlash(btn) {
+    btn.classList.add("userFlash"); 
+    setTimeout(function() {
+        btn.classList.remove("userFlash")
+     }, 250);
+}
+
+function gameFlash(btn) {
     btn.classList.add("flash"); 
     setTimeout(function() {
         btn.classList.remove("flash")
@@ -26,17 +34,59 @@ function btnFlash(btn) {
 }
 
 function levelUp() {
+    userSeq = [];
     level ++ ;
     h2.innerText = `Level ${level}`;
 
     let randIdx = Math.floor(Math.random()*3);  
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randbtn);
-    btnFlash(randbtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq)
+    gameFlash(randbtn);
 
 }
 
+function checkAns(idx) {
+    if(userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+
+    } else {
+        if(level > highScore) {
+            highScore = level;
+        }
+        h2.innerHTML = `Game Over !Your score was <b>${level}</b><br>High Score : ${highScore} <br> Press any key to Start.`;
+        document.querySelector('body').style.backgroundColor = "Red";
+        setTimeout(function () {
+            document.querySelector('body').style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
+
+}
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0 ;
+
+}
+
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id") ;
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length -1);
+}
+
+
+let allbtn = document.querySelectorAll(".btn");
+for (btn of allbtn) {
+    btn.addEventListener("click", btnPress);
+}
 
