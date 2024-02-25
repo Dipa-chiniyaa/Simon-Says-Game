ed5f534b27f2de2a36c03f798b7c6eb7c8047051
Dipa Chiniya Parasaram <157943824+Dipa-chiniyaa@users.noreply.github.com>
2024-02-25 11:00:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-let gameSeq = [];
-let userSeq = [];
-
-let btns = ['yellow','purple', 'red', 'green']; // manipulating the buttons by their classes
-
-let started = false ;
-let level = 0 ;
-
-let h2 = document.querySelector("h2");
-
-document.addEventListener('keypress', function () {
-    if(started == false) {
-        console.log("game has started.");
-        started = true ;
-
-        levelUp();
-
-    }
-});
-
-function btnFlash(btn) {
-    btn.classList.add("flash"); 
-    setTimeout(function() {
-        btn.classList.remove("flash")
-     }, 250);
-}
-
 function levelUp() {
+    userSeq = [];
     level ++ ;
     h2.innerText = `Level ${level}`;
 
     let randIdx = Math.floor(Math.random()*3);  
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randbtn);
-    btnFlash(randbtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq)
+    gameFlash(randbtn);
 
 }
 
+function checkAns(idx) {
+   
+    if(userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
 
+    } else {
+        h2.innerText = "Game Over ! Press any key to Start.";
+    }
+
+}
